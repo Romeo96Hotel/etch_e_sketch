e87f492e39185e4b6b257961cf1269a1798771ff
romeo hotel <90534544+Romeo96Hotel@users.noreply.github.com>
2023-01-26 16:59:58 -0300

    Changed color

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ for (let i = 0; i < 256; i++) {
   const container = document.querySelector(".container");
   const grid = document.createElement("div");
   grid.id = "square";
-  grid.innerHTML = ".";
-
   grid.addEventListener("mouseover", () => {
-    grid.style.backgroundColor = "cyan";
+    grid.style.backgroundColor = "black";
   });
 
   container.appendChild(grid);
