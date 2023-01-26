a39c23ad8f64908fa1ec9e2305b9d2419c035c50
romeo hotel <90534544+Romeo96Hotel@users.noreply.github.com>
2023-01-26 16:44:10 -0300

    Added hover action listener

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ for (let i = 0; i < 256; i++) {
   const container = document.querySelector(".container");
   const grid = document.createElement("div");
   grid.id = "square";
-  grid.textContent = ".";
+  grid.innerHTML = ".";
+
+  grid.addEventListener("mouseover", () => {
+    grid.style.backgroundColor = "cyan";
+  });
+
   container.appendChild(grid);
 }
