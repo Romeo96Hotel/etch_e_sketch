2f7496f5e3fa99e8189a563b1b1bd6fdfffff1ce
romeo hotel <90534544+Romeo96Hotel@users.noreply.github.com>
2023-01-27 16:59:04 -0300

    Created function to render grid

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const container = document.querySelector(".container");
 const gridButton = document.querySelector("#grid-btn");
-let gridSize;
+const sizeBtn = document.querySelector("#grid-btn");
+const userInput = prompt("choose grid size");
 
-/* standard 16 x 16 grid */
-for (let i = 0; i < 256; i++) {
-  const grid = document.createElement("div");
-  grid.id = "square";
-  grid.addEventListener("mouseover", () => {
-    grid.style.backgroundColor = "black";
-  });
-  container.appendChild(grid);
+
+function renderGrid(size) {
+  size = parseInt(userInput);
+  /* standard 16 x 16 grid */
+  for (let i = 0; i < 256; i++) {
+    const grid = document.createElement("div");
+    grid.id = "square";
+    grid.addEventListener("mouseover", () => {
+      grid.style.backgroundColor = "black";
+    });
+    container.appendChild(grid);
+  }
 }
