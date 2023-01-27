10994276c12ad7a72dafebbe33817b8a862e0a77
romeo hotel <90534544+Romeo96Hotel@users.noreply.github.com>
2023-01-27 16:16:56 -0300

    Removed function for user prompt

--- index.js
+++ index.js
@@ -2,21 +2,19 @@ const container = document.querySelector(".container");
 const gridButton = document.querySelector("#grid-btn");
 
 /* standard 16 x 16 grid */
-function standardGrid() {
-  for (let i = 0; i < 256; i++) {
-    const grid = document.createElement("div");
-    grid.id = "square";
-    grid.addEventListener("mouseover", () => {
-      grid.style.backgroundColor = "black";
-    });
-    container.appendChild(grid);
-  }
+for (let i = 0; i < 256; i++) {
+  const grid = document.createElement("div");
+  grid.id = "square";
+  grid.addEventListener("mouseover", () => {
+    grid.style.backgroundColor = "black";
+  });
+  container.appendChild(grid);
 }
 
-/* returns grid size based on inputted number */
-function getGridSize() {
-  gridButton.addEventListener("click", () => {
-    const userInput = prompt("select grid size");
-    return userInput;
-  });
+/* gets users input from promp and creats a grid of that size */
+function setGridSize(size) {
+  size = getGridSize();
+  console.log(size);
 }
+
+setGridSize();
