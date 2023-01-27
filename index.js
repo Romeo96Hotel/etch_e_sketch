744c53fbec198d7e29a61f99c4329f4de092cfd7
romeo hotel <90534544+Romeo96Hotel@users.noreply.github.com>
2023-01-27 15:56:23 -0300

    Added button to allow change to grid size Changed function for standard grid size Added function to return users input

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-for (let i = 0; i < 256; i++) {
-  const container = document.querySelector(".container");
-  const grid = document.createElement("div");
-  grid.id = "square";
-  grid.addEventListener("mouseover", () => {
-    grid.style.backgroundColor = "black";
-  });
+const container = document.querySelector(".container");
+const gridButton = document.querySelector("#grid-btn");
 
-  container.appendChild(grid);
+/* standard 16 x 16 grid */
+function standardGrid() {
+  for (let i = 0; i < 256; i++) {
+    const grid = document.createElement("div");
+    grid.id = "square";
+    grid.addEventListener("mouseover", () => {
+      grid.style.backgroundColor = "black";
+    });
+    container.appendChild(grid);
+  }
+}
+
+/* returns grid size based on inputted number */
+function getGridSize() {
+  gridButton.addEventListener("click", () => {
+    const userInput = prompt("select grid size");
+    return userInput;
+  });
 }
