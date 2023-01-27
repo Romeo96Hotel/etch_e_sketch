5ccb55893602052d161af2ec5f5e044b1a531602
romeo hotel <90534544+Romeo96Hotel@users.noreply.github.com>
2023-01-27 20:50:32 -0300

    Got renderGrid function to work

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 const container = document.querySelector(".container");
-const gridButton = document.querySelector("#grid-btn");
-const sizeBtn = document.querySelector("#grid-btn");
-const colorBtn = document.querySelector("#rbw-btn");
+const sizeBtn = document.querySelector("#size-btn");
+const colorBtn = document.querySelector("#color-btn");
+let gridSize;
+
+/* Button renders new grid with specified size */
+sizeBtn.addEventListener("click", () => {
+  const userInput = parseInt(prompt("set grid size"));
+  gridSize = userInput;
+  renderGrid(gridSize);
+});
+
+/* Button renders gridsquares with specified color on hover */
+sizeBtn.addEventListener("click", () => {
+  const userInput = parseInt(prompt("set grid size"));
+  gridSize = userInput;
+  renderGrid(gridSize);
+});
 
 /* Returns a 500px by 500px square grid */
 /* Number of tiles is specified by user */
-function setGridSize(numTiles) {
+function renderGrid(numTiles) {
   container.style.display = "grid";
   container.style.gridTemplateColumns = `repeat(${numTiles}, minmax(auto, auto))`;
   container.style.gridTemplateRows = `repeat(${numTiles}, minmax(auto, auto))`;
   container.style.height = "500px";
   container.style.width = "500px";
+  container.style.backgroundColor = "gray";
   const area = numTiles * numTiles;
 
   for (let i = 0; i < area; i++) {
-    const grid = document.createElement("div");
-    return grid;
+    const gridSquare = document.createElement("div");
+    gridSquare.addEventListener("mouseover", () => {
+      gridSquare.style.backgroundColor = "red";
+    });
+    container.appendChild(gridSquare);
   }
 }
 
@@ -23,4 +41,3 @@ function setGridSize(numTiles) {
 function setDrawColor() {}
 
 /* Etch-e-Sketch */
-setGridSize();
