4a88a0aac437425ec1dd298fcdce9864200dd0d2
romeo hotel <90534544+Romeo96Hotel@users.noreply.github.com>
2023-01-27 23:02:21 -0300

    Disregard

--- index.js
+++ index.js
@@ -1,37 +1,26 @@
 const container = document.querySelector(".container");
 const sizeBtn = document.querySelector("#size-btn");
-const colorBtn = document.querySelector("#color-btn");
-let gridSize;
 
 /* Button renders new grid with specified size */
-sizeBtn.addEventListener("click", () => {
-  const userInput = parseInt(prompt("set grid size"));
-  gridSize = userInput;
-  renderGrid(gridSize);
-});
+sizeBtn.addEventListener("click", renderGrid);
 
-/* Button renders gridsquares with specified color on hover */
-sizeBtn.addEventListener("click", () => {
-  const userInput = parseInt(prompt("set grid size"));
-  gridSize = userInput;
-  renderGrid(gridSize);
-});
-
-/* Returns a 500px by 500px square grid */
+/* Returns a 500 x 500 square grid */
 /* Number of tiles is specified by user */
-function renderGrid(numTiles) {
+function renderGrid() {
+  const numTiles = prompt("Set grid size");
+  const area = numTiles * numTiles;
+
   container.style.display = "grid";
   container.style.gridTemplateColumns = `repeat(${numTiles}, minmax(auto, auto))`;
   container.style.gridTemplateRows = `repeat(${numTiles}, minmax(auto, auto))`;
   container.style.height = "500px";
   container.style.width = "500px";
   container.style.backgroundColor = "gray";
-  const area = numTiles * numTiles;
 
   for (let i = 0; i < area; i++) {
     const gridSquare = document.createElement("div");
     gridSquare.addEventListener("mouseover", () => {
-      gridSquare.style.backgroundColor = "red";
+      gridSquare.style.backgroundColor = penColor;
     });
     container.appendChild(gridSquare);
   }
